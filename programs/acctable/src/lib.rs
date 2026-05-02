a4c70d25d47a72e22a172a0901ddd590039d2e54
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2026-05-02 17:29:10 +0100

    add lock and unlock instructions to lib.rs

--- programs/acctable/src/lib.rs
+++ programs/acctable/src/lib.rs
@@ -1,6 +1,11 @@
 use anchor_lang::prelude::*;
+pub mod errors;
 pub mod instructions;
 pub mod state;
+
+pub use instructions::*;
+pub use crate::state::LockState;
+
 declare_id!("9U2SnYkHeWhQHsteHZBGb2W776MrahyWXTuvZhQoQwbi");
 
 #[program]
@@ -9,6 +14,16 @@ pub mod acctable {
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn lock(ctx: Context<Lock>, amt: u64) -> Result<()> {
+        instructions::lock::lock_handler(ctx, amt)?;
+        Ok(())
+    }
+
+    pub fn unlock(ctx: Context<Unlock>) -> Result<()> {
+        instructions::unlock::unlock_handler(ctx)?;
         Ok(())
     }
 }
